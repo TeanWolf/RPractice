646b3d2c09f731053cc91cc54c9cbf316a7628a5
Valentin <valentinminin44@gmail.com>
2026-01-27 20:36:47 +0500

    Пройдены переменные

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a1 int = 52
+	var a2 string = "FALSE"
+	var a3 float64 = 5.3
+	var a4 bool = true
+
+	a1 = 23
+	a2 = "TRUE"
+	a3 = 5.8
+	a4 = false
+
+	fmt.Println("a1:", a1, " a2:", a2, " a3:", a3, " a4:", a4)
+}
